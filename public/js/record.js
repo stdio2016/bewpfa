dfdfbd1a1b13ad38934b08c6f1974c4e2f9a1a84
stdio2016 <randomfrank8@gmail.com>
2020-11-15 23:00:31 +0800

    add to tbody

--- public/js/record.js
+++ public/js/record.js
@@ -263,18 +263,22 @@ function ended() {
 
 function showResult(json) {
   var songs = json.songs;
-  var table = document.querySelector('.query-results table');
+  var table = document.querySelector('.query-results table tbody');
   console.log(table);
-  while (table.rows.length > 1) {
-    table.rows[1].remove();
+  while (table.rows.length > 0) {
+    table.rows[0].remove();
   }
   for (var i = 0; i < songs.length; i++) {
-    var row = table.insertRow(1);
+    var row = table.insertRow(i);
     var cell1 = row.insertCell(0);
     var cell2 = row.insertCell(1);
+    var cell3 = row.insertCell(2);
     cell1.textContent = songs[i].name;
     cell2.textContent = songs[i].score;
     cell2.style.textAlign = 'center';
+    var audio = new Audio('/finddup/audio/' + songs[i].name);
+    audio.controls = true;
+    cell3.append(audio);
   }
 }
 
